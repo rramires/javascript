178575ba9cf821c7642c6650bfcc36c99a460db6
rramires <ricardo@flexbr.com>
2021-07-17 16:21:42 -0300

    Renomeando pasta

--- 01_Fundamentos/12_funcoes_em_tudo.js
+++
@@ -1,9 +0,0 @@
-// Um objeto tambem é uma funcao
-console.log(typeof Object)
-
-// declarando uma classe
-class Produto{}
-
-// Uma classe tambem é uma funcao
-console.log(typeof Produto)
-
--- 01_Fundamentos/18_usando_var_em_loop_1.js
+++
@@ -1,9 +0,0 @@
-for(var i = 0; i < 10; i++)
-{
-    console.log(i)
-}
-// quando passa de 10 sai do laco
-// com o var a variavel fica acessivel
-console.log('Valor final do i: ' + i)
-
-// veja diferenca em usando_let_em_loop_1.js
