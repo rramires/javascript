261a265da39cf89fac0a1cb582d570dec4159594
rramires <ricardo@flexbr.com>
2021-09-21 18:32:05 -0300

    Reforço de Promise 02

--- 07_ESNext/11_reforco_promise_1.js
+++ 07_ESNext/11_reforco_promise_1.js
@@ -13,7 +13,7 @@ Promises servem para encadear execução de código de forma elegante
 /**
  * Ex 1: Criamos uma Promise que retorna um array com 3 elementos
  */
-let p1 = new Promise((resolve) => {
+let p1 = new Promise(resolve => {
     // retorna para a função que é passada
     resolve(['Fulano', 'Sicrano', 'Beltrano'])
 })
@@ -26,7 +26,7 @@ let p1 = new Promise((resolve) => {
  */ 
 // Exemplos:
 // 1 - Nesse caso foi passado uma função simples, que apenas retorna 
-console.log( p1.then(nomes => nomes) ) // Retornou ['Fulano', 'Sicrano', 'Beltrano']
+p1.then(nomes => console.log(nomes)) // Retornou ['Fulano', 'Sicrano', 'Beltrano']
 
 // 2 - Encadeando
 p1.then(nomes => nomes[1]) // retorna o segundo nome: Sicrano
@@ -34,7 +34,7 @@ p1.then(nomes => nomes[1]) // retorna o segundo nome: Sicrano
   .then(letra => letra.toLowerCase()) // retorna: s em caixa baixa
   .then(l => console.log(l)) // imprime: s
 
-// 2 - Fazendo a mesma coisa de maneira mais elegante
+// 3 - Fazendo a mesma coisa de maneira mais elegante
 // com funcoes reaproveitáveis:
 const getFirst = value => value[0]
 const getSecond = value => value[1]
